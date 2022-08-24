2c1a592dc6132ce9f47d3dff6106c2ec17a8cb7f
Olin Johnson <olinlj@icloud.com>
2022-08-23 23:34:17 -0400

    Added course assignments

--- student-schedule.py
+++ student-schedule.py
@@ -3,9 +3,11 @@ import random
 
 periods = 8
 
-def main():
+def assign_classes():
     classes = pandas.read_csv("./course-list.csv")
     students = pandas.read_csv("./student-names-reduced.csv")
+
+    assigned_classes = pandas.DataFrame(columns=["Name", "Period 1", "Period 2", "Period 3", "Period 4", "Period 5", "Period 6", "Period 7", "Period 8"])
 
     for i in range(0, len(students)):
         chosen_courses = []
@@ -14,10 +16,16 @@ def main():
             if(course in chosen_courses):
                 continue
             else:
-                chosen_courses[len(chosen_courses)] = course
+                chosen_courses.append(course)
+        
+        assigned_classes.loc[i] = [students["name"][i]] + chosen_courses
 
-    print("LEN STUDENTS: ", len(students))
+    print("Classes assigned. View them in course-assignments.csv")
+    assigned_classes.to_csv("./course-assignments.csv")
 
+
+def main():
+    assign_classes()
 
 if __name__ == "__main__":
     main()
